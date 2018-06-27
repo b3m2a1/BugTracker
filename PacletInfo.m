4be3937ff0245ba9bdd21246c7682e6025fc0fe7
b3m2a1 <b3m2a1@gmail.com>
2018-06-27 02:33:16 -0700

    Committed BugTracker application @ 2018-06-27_02:33:16

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,6 +1,10 @@
+(* ::Package:: *)
+
 Paclet[
   Name -> "BugTracker",
   Version -> "1.0.2",
+  Creator->"b3m2a1 <b3m2a1@gmail.com>",
+  URL->"https://github.com/b3m2a1/BugTracker",
   Extensions -> {
     	{
      		"Kernel",
@@ -9,8 +13,14 @@ Paclet[
      	},
     	{
      		"FrontEnd",
-     		"Prepend" -> True,
-     		Prepend -> True
+     		"Prepend" -> True
+     	},
+    	{
+     		"PacletServer",
+     		"Description" -> "A little framework for tracking bugs",
+     		"License" -> "MIT",
+     		"Tags" -> {"bugs"},
+     		"Categories" -> {"Development"}
      	}
     }
  ]
